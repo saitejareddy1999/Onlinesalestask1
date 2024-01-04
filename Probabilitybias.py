b4e6a996e0573dec97f4e10cea5a9f61336ff50d
saitejareddy1999 <saitejareddy1999@gmail.com>
2024-01-04 12:47:52 +0530

    this is first commit

---
+++ Probabilitybias.py
@@ -0,0 +1,25 @@
+# they are asking if we roll a dice or else if we do flip a coin have given
+# occurrences we have to calculate how many times a particular one is coming
+
+# we want random outcomes we are importing random module
+import random
+
+
+def event(probabilities, occurrences):
+    outcomes = []  # we are getting how many for each input
+    for probability_dict in probabilities:
+        for outcome, probability in probability_dict.items():
+            for _ in range(probability):
+                outcomes.append(outcome)
+    results = {}
+    for _ in range(occurrences):
+        outcome = random.choice(outcomes)
+        results[outcome] = results.get(outcome, 0) + 1
+    return results
+
+
+coin_bias = [{1: 35}, {2: 65}]
+occurrences = eval(input())
+result = event(coin_bias, occurrences)
+print(result)
+
